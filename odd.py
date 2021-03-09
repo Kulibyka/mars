c61305a819003e5a5d3f34f30bebc997435392c4
Kulibyka <59702274+Kulibyka@users.noreply.github.com>
2021-03-09 18:10:00 +0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/index/<title>')
 def login(title):
     return render_template('base.html', title=title)
 
