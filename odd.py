fdb3acdcfaa72e4fffc939abdf1f4b3325e0ec24
Kulibyka <59702274+Kulibyka@users.noreply.github.com>
2021-03-10 10:49:50 +0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -9,5 +9,10 @@ def login(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
